ebf1c427029ebc3a4e8db65d1788eec2e6f5decd
alekhakul <alekhakul@gmail.com>
2025-08-02 07:43:34 -0500

    First Commit

---
+++ src/main.cpp
@@ -0,0 +1,86 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal.h>
+#include <SensirionI2CSen5x.h>
+
+
+// Initialize LCD with pin numbers
+LiquidCrystal lcd(12, 13, 14, 2, 0, 15);
+
+// Sensor Object
+SensirionI2CSen5x sen55;
+
+// Data variables
+float massConcentrationPm1p0, massConcentrationPm2p5;
+float massConcentrationPm4p0, massConcentrationPm10p0;
+float ambientHumidity, ambientTemperature;
+float vocIndex, noxIndex;
+
+void setup() {
+    Serial.begin(115200);
+
+    Wire.begin();
+
+    // Initialize LCD
+    lcd.begin(16, 2);
+    lcd.print("Starting...");
+
+    // Initialize sensor
+    sen55.begin(Wire);
+
+    // Start measurement, check for errors
+    uint16_t error = sen55.startMeasurement();
+    if (error) {
+        Serial.print("Error starting measurement:");
+        char errorMessage[256];
+        errorToString(error, errorMessage, 256);
+        Serial.println(errorMessage);
+
+        lcd.clear();
+        lcd.print("Sensor Error");
+        while(true) {}
+    }
+
+    delay(1000);
+}
+
+void loop() {
+    // Wait to prepare data
+    delay(1000);
+
+    // Read measurement data, check for errors
+    uint16_t error = sen55.readMeasuredValues(
+        massConcentrationPm1p0, massConcentrationPm2p5,
+        massConcentrationPm4p0, massConcentrationPm10p0,
+        ambientHumidity, ambientTemperature, vocIndex, noxIndex);
+    // Check for errors
+    if (error) {
+        Serial.print("Error reading vals:");
+        char errorMessage[256];
+        errorToString(error, errorMessage, 256);
+        Serial.println(errorMessage);
+    } else {
+        // Display values
+        lcd.clear();
+
+        // Temperature
+        lcd.setCursor(0, 0);
+        lcd.print(ambientTemperature);
+        lcd.print((char)223);
+        lcd.print("C ");
+
+        // Humidity
+        lcd.setCursor(9, 0);
+        lcd.print(ambientHumidity,0);
+        lcd.print("% RH");
+
+        // Mass Concentration PM2.5
+        lcd.setCursor(0, 1);
+        lcd.print(massConcentrationPm2p5,0);
+
+        // VOC Index
+        lcd.setCursor(10, 1);
+        lcd.print("VOC:");
+        lcd.print(vocIndex, 0);
+    }
+}
